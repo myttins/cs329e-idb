145ff86e9e3c67d4f924a422ff6dc50a6e5f9c76
amv2768 <viraniamaan@gmail.com>
2019-03-11 15:04:20 -0500

    Created initial pages for books, authors, publishers

--- main.py
+++ main.py
@@ -4,12 +4,23 @@
 #-----------------------------------------
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def index():
 	return render_template('hello.html')
-@app.route('/book/')
-def book():
-	return render_template('book.html')
+
+@app.route('/books/')
+def bookw():
+	return render_template('books.html')
+
+@app.route('/publishers/')
+def publishers():
+	return render_template('publishers.html')
+
+@app.route('/authors/')
+def authors():
+	return render_template('authors.html')
+
 if __name__ == "__main__":
 	app.run()
 #----------------------------------------
