6f67d5eb1f24b8978be7811a9782eb11b4519634
amv2768 <viraniamaan@gmail.com>
2019-03-11 20:37:35 -0500

    Created base about and index pages

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask_nav import Nav
 from flask_nav.elements import *
 nav = Nav()
 nav.register_element('top', Navbar(
-    View('Home', 'index'),
+    View('Home', 'index'),View('About', 'about'),
     View('Books', 'books'), View('Publishers','publishers'),View('Authors','authors')))
 app = Flask(__name__)
 nav.init_app(app)
@@ -17,7 +17,11 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-	return render_template('hello.html')
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 @app.route('/books/')
 def books():
