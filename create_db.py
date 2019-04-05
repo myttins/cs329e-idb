8fb883799b279614c42eb9a791e74cd9dedf3cd8
Raza Retiwala <rretiwala@utexas.edu>
2019-04-05 02:47:55 +0000

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql+psycopg2://postgres:password@localhost/booksdb')
+engine = create_engine('postgresql+psycopg2://postgres:password@/postgres?host=/cloudsql/cs-329e-website:us-central1:booksdb')
 Base = declarative_base()
 Base.metadata.create_all(engine)
 file_name = 'books_only.csv'
